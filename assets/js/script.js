2ed787f864f895e2e2d267adaeb68823382331f0
sean2073 <thatbrowngurl@gmail.com>
2021-12-06 18:35:45 -0500

    feature/form-submit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,20 @@
-var buttonEl = document.querySelector("#save-task");
+// var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var taskEl = document.getElementById("tasks-to-do");
-console.log(buttonEl);
+// console.log(buttonEl);
 
-  var createTaskHandler = function(){
+  var createTaskHandler = function(event){
+    event.preventDefault();
       console.log("button clicked");
     var taskItemEl = document.createElement("li");
-    taskItemEl.textContent = "This is a new task.";
+
+
+    // taskItemEl.textContent = "This is a new task.";
+    taskItemEl.textContent = taskEl.value;
     taskItemEl.setAttribute("class", "task-item")
     taskEl.appendChild(taskItemEl);
+    console.log(taskItemEl);
 
   };
-  buttonEl.addEventListener("click", createTaskHandler);
+//   buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
